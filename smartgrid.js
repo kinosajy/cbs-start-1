830fe86a058628d4b818bf33ee7752bad399f893
kinosajy <makko@ukr.net>
2019-05-10 17:52:29 +0300

    test

---
+++ smartgrid.js
@@ -0,0 +1,22 @@
+const smartgrid = require('smart-grid');
+
+const settings = {
+  columns: 12,
+  offset: '30px',
+  container: {
+    maxwidth: '1170px',
+    fields: '30px'
+  },
+  breakPoints: {
+
+  },
+  oldSiteStyle: false,
+  properties: [],
+  mixinNames: {
+    column: 'col',
+    container: 'wrapper',
+    row: 'row',
+    size: 'size',
+    offset: 'offset'
+  }
+}
